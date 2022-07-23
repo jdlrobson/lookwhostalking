29a8afdd49080855e409c620a20bb56d71d1f159
Jon Robson <jdlrobson@gmail.com>
2022-07-22 17:54:21 -0700

    Separate common fetch function into util.js

--- populate.js
+++ populate.js
@@ -4,16 +4,17 @@ const topics = {
     sections: []
 };
 const WAIT_INC = _cachedOnly ? 0 : 500;
-const fetch = require( 'node-fetch');
 const fs = require( 'fs' );
 const TOPIC_PATH = `${__dirname}/topics.json`;
-const FETCH_CACHE_PATH = `${__dirname}/.fetchCache.json`;
 const TOPIC_CACHE_PATH = `${__dirname}/.topicCache.json`;
-const fetchCache = fs.existsSync( FETCH_CACHE_PATH ) ?
-    JSON.parse(fs.readFileSync( FETCH_CACHE_PATH ).toString()) : {};
 const topicCache = fs.existsSync( TOPIC_CACHE_PATH ) ?
     JSON.parse(fs.readFileSync( TOPIC_CACHE_PATH ).toString()) : {};
-const MAX_FETCHES = 100;
+
+const util = require( './util.js' );
+const cachedFetch = util.cachedFetch;
+const saveCache = util.saveCache;
+const tooManyRequests = util.tooManyRequests;
+
 /*
 projects.json: https://www.wikidata.org/wiki/Q4582194
 var getsite = (x) => {  const url = x.replace( 'wik', '.wik' ).replace( 'wiktionary', 'wiktionary.org' ).replace('commons.wiki', 'commons.wikipedia.org').replace('meta.wiki', 'meta.wikimedia.org').replace('.wikidatawiki', 'www.wikidata.org').replace('media.wiki.wiki', 'www.mediawiki.org').replace('.wikivoyage', 'wikivoyage.org').replace('.wikiversity', '.wikiversity.org').replace('wikinews', 'wikinews.org').replace('wikiquote','wikiquote.org').replace('wikibooks', 'wikibooks.org'); return url.indexOf('.org') === -1 ? url + 'pedia.org' : url }
@@ -29,56 +30,6 @@ const waitFor = ( time ) => {
         setTimeout( () => resolve(), time )
     });
 };
-
-const addMinsToDate = ( mins, toDate ) => {
-   const date = toDate ? new Date( toDate ) : new Date();
-    date.setTime( date.getTime() + ( mins * 60 * 1000 ) );
-    return date;
-};
-
-const saveCache = ( path, json ) => {
-    fs.writeFileSync(
-        path,
-        JSON.stringify( json )
-    );
-};
-
-const getExpiryDate = ( url ) => {
-    if ( url.indexOf( 'wikidata.org' ) > -1 || url.indexOf( 'commons.wikimedia.org' ) > -1 ) {
-        return ( 12 * 60 );
-    } else if ( url.indexOf( '.wikipedia.org' ) > -1 ) {
-        return ( 3 * 60 );
-    } else {
-        return ( 24 * 60 );
-    }
-};
-
-let numberOfFetchesInThisSession = 0;
-const tooManyRequests = () => numberOfFetchesInThisSession > MAX_FETCHES;
-const now = new Date();
-
-const cachedFetch = ( url ) => {
-    const cachedResult = fetchCache[url];
-    if ( cachedResult || _cachedOnly ) {
-        if ( now < cachedResult.expires || tooManyRequests() ) {
-            console.log('(Loaded from cache)')
-            return Promise.resolve( cachedResult.json );
-        }
-    }
-    if ( tooManyRequests() ) {
-        console.log( 'Refusing - too many requests' );
-        return Promise.reject();
-    }
-    console.log('(Fetching from server)')
-    numberOfFetchesInThisSession++;
-    return fetch( url ).then((r)=>r.json()).then((json) => {
-        // should expire anywhere between 1-2hrs
-        const mins = getExpiryDate( url ) + ( Math.random() * 60 );
-        fetchCache[url] = { json, expires: addMinsToDate( mins ) };
-        saveCache( FETCH_CACHE_PATH, fetchCache );
-        return json;
-    });
-}
 
 let waitTime = 0;
 
---
+++ util.js
@@ -0,0 +1,57 @@
+const fs = require( 'fs' );
+const fetch = require( 'node-fetch');
+const FETCH_CACHE_PATH = `${__dirname}/.fetchCache.json`;
+const fetchCache = fs.existsSync( FETCH_CACHE_PATH ) ?
+JSON.parse(fs.readFileSync( FETCH_CACHE_PATH ).toString()) : {};
+const MAX_FETCHES = 1;
+
+const saveCache = ( path, json ) => {
+    fs.writeFileSync(
+        path,
+        JSON.stringify( json )
+    );
+};
+
+const addMinsToDate = ( mins, toDate ) => {
+    const date = toDate ? new Date( toDate ) : new Date();
+    date.setTime( date.getTime() + ( mins * 60 * 1000 ) );
+    return date;
+};
+
+const getExpiryDate = ( url ) => {
+    if ( url.indexOf( 'wikidata.org' ) > -1 || url.indexOf( 'commons.wikimedia.org' ) > -1 ) {
+        return ( 12 * 60 );
+    } else if ( url.indexOf( '.wikipedia.org' ) > -1 ) {
+        return ( 3 * 60 );
+    } else {
+        return ( 24 * 60 );
+    }
+};
+
+let numberOfFetchesInThisSession = 0;
+const tooManyRequests = () => numberOfFetchesInThisSession > MAX_FETCHES;
+const now = new Date();
+const cachedFetch = ( url, cachedOnly ) => {
+    const cachedResult = fetchCache[url];
+    if ( cachedResult || cachedOnly ) {
+        if ( now < cachedResult.expires || tooManyRequests() ) {
+            console.log('(Loaded from cache)')
+            return Promise.resolve( cachedResult.json );
+        }
+    }
+    if ( tooManyRequests() ) {
+        console.log( 'Refusing: too many requests' );
+        return Promise.reject();
+    }
+    console.log('(Fetching from server)')
+    numberOfFetchesInThisSession++;
+    return fetch( url ).then((r)=>r.json()).then((json) => {
+        // should expire anywhere between 1-2hrs
+        const mins = getExpiryDate( url ) + ( Math.random() * 60 );
+        fetchCache[url] = { json, expires: addMinsToDate( mins ) };
+        saveCache( FETCH_CACHE_PATH, fetchCache );
+        return json;
+    });
+}
+
+module.exports = { cachedFetch, tooManyRequests, saveCache };
